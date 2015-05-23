a3ba7a3e4a994430cc33930b6b3d003aef3ee69d
Austin Wu <austin.wu@duke.edu>
2015-05-23 09:30:34 -0500

    Functioning node/express/angular framework for weather api call

--- public/javascripts/bulletinApp.js
+++ public/javascripts/bulletinApp.js
@@ -1,6 +1,95 @@
-angular.module('bulletin', [])
+var app = angular.module('bulletin',['ui.router']);
+
+app.config([
+'$stateProvider',
+'$urlRouterProvider',
+function($stateProvider, $urlRouterProvider) {
+
+	$stateProvider
+	    .state('home', {
+	      url: '/home',
+	      templateUrl: '/home.html',
+	      controller: 'MainCtrl'
+	    })
+
+$urlRouterProvider.otherwise('home');
+}]);
+
+app.controller('MainCtrl', 
+	function($scope, $http) {
+  		$http.get('forecast').then(function(data){
+  			$scope.temperature = data.data.currently.temperature;
+  			$scope.longitude= data.data.longitude;
+  			$scope.latitude = data.data.latitude;
+  		})
+});
+
+
+
+/*
+
+
+angular.module('bulletin', ['ui.router'])
+.config([
+'$stateProvider',
+'$urlRouterProvider',
+function($stateProvider, $urlRouterProvider) {
+
+	$stateProvider
+	    .state('home', {
+	      url: '/home',
+	      templateUrl: '/home.html',
+	      controller: 'MainCtrl'
+	    })
+
+$urlRouterProvider.otherwise('home');
+}]);
+
+.factory('forecast', ['$http', function($http){
+	var o = {};
+	o.getForecast = function(){
+		return $http.get('/forecast').then(function(res){
+			angular.copy(data, o);
+		})
+	}
+	return o;
+}]);
+
 .controller('MainCtrl', [
-'$scope',
+'$scope', 
 function($scope){
   $scope.test = 'Hello this is bulletin!';
+  $scope.temperature = -99;
 }]);
+
+app.factory('forecast', ['$http', function($http){
+	var weather = {latitude: 3};
+	var input = {temperature: 10};
+
+	return {
+		weather: function(){
+			return weather.latitude;
+
+		},
+		temperature: function(){
+			return input.temperature;
+		}
+	}
+}])
+
+app.controller('MainCtrl', [
+	'$scope', 
+	'forecast',
+	function($scope, forecast, $http) {
+  		$http.get('forecast').then(function(data){
+  			$scope.weather = data;
+  		})
+  		$scope.test = 'Hello World!';
+  		$scope.temperature = forecast.temperature();
+  		
+
+}]);
+
+*/
+
+
--- routes/index.js
+++ routes/index.js
@@ -1,23 +1,29 @@
 var express = require('express');
 var router = express.Router();
+var forecast = require('forecast');
 
-// Require the module 
-var Forecast = require('forecast');
- 
-// Initialize 
-var forecast = new Forecast({
-  service: 'forecast.io',
-  key: '9cfdff0868949f528506fc3da052c243',
-  units: 'celcius', // Only the first letter is parsed 
-});
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-	forecast.get([42.153611, -88.131944], true, function(err, weather) {
-  		if(err) return console.dir(err);
-  		var temp = weather.currently.temperature;
-  		res.render('index', { title: 'Bulletin', temp: temp});
+	res.render('index', { title: 'Bulletin' });
+	console.log("calling router.get home page");
+});
+
+var forecast = new forecast({
+  service: 'forecast.io',
+  key: '9cfdff0868949f528506fc3da052c243',
+  units: 'c', // Only the first letter is parsed
+});
+
+/* GET forecast */
+router.get('/forecast', function(req, res, next) {
+	forecast.get([42.083012, -88.147655], true, function(err, weather) {
+	  	if(err) return console.dir(err);
+		res.json(weather);
+		console.log(weather);
 	});
-}); 
- 
+});
+
+
 module.exports = router;
+
